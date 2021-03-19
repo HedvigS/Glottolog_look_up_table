dc027bfebc7b3582daf0d4af49d63031be459a74
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-03-19 16:01:36 +0100

    place groundhog_packages in working dir folder

--- requirements.R
+++ requirements.R
@@ -1,7 +1,13 @@
 # installs the necessary packages for these scripts
 
+day="2021-03-14" #the particular date which we want to re-create, we'll be loading packages as they were on this specific date on CRAN.
+
 if (!suppressPackageStartupMessages(require('groundhog'))) { install.packages('groundhog') }
 library(groundhog)
+
+if (!dir.exists("groundhog_packages")) { dir.create("groundhog_packages") }
+
+set.groundhog.folder("groundhog_packages") #place the date-specific packages in a folder inside this directory instead of somewhere higher up in the folder hierarchy. 
 
 pkgs <- c(
   'fields', #for geo distance calculations
@@ -20,5 +26,4 @@ pkgs <- c(
   'jsonlite'
   )
 
-day="2021-03-14"
 groundhog.library(pkgs, day)
