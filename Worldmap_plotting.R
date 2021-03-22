690a1a58ab54b984a681afabed34e6acb3113db9
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-03-22 15:39:03 +0100

    using cldf instead of own table

--- Worldmap_plotting.R
+++ Worldmap_plotting.R
@@ -1,6 +1,21 @@
 source("requirements.R")
 
-dir.create("plots")
+if (!dir.exists("plots")) { dir.create("plots") }
+
+values <- read_csv("https://raw.githubusercontent.com/glottolog/glottolog-cldf/master/cldf/values.csv", na = c("","<NA>")) %>% 
+  rename(Glottocode = Language_ID)
+
+values_wide <- values %>% 
+  dcast(Glottocode ~ Parameter_ID, value.var = "Value")
+
+languages <- read_csv("https://raw.githubusercontent.com/glottolog/glottolog-cldf/master/cldf/languages.csv", na = c("","<NA>")) %>% 
+  dplyr::select(-ID) %>% 
+  rename(Language_level_ID = Language_ID) 
+
+Glottolog <- full_join(values_wide,languages) %>% 
+  mutate(Language_level_ID = ifelse(level == "language", Glottocode, Language_level_ID))
+
+rm(values, languages, values_wide)
 
 #worldmaps
 #rendering a worldmap that is pacific centered
@@ -8,21 +23,26 @@ world <- map_data('world', wrap=c(-25,335), ylim=c(-56,80), margin=T)
 
 lakes <- map_data("lakes", wrap=c(-25,335), col="white", border="gray", ylim=c(-55,65), margin=T)
 
-Glottolog <- read_tsv("Glottolog_lookup_table_Hedvig_output/Heti_Glottolog_lookup_table_cldf_version.tsv") %>% 
-  filter(level == "language") 
+Glottolog_only_languages <- Glottolog %>%
+  filter(Family_ID != 'book1242') %>% 
+  filter(Family_ID != 'unat1236') %>%
+  filter(Family_ID != 'arti1236') %>% 
+  filter(Family_ID != 'pidg1258') %>% 
+  filter(Family_ID != 'spee1234') %>% 
+  filter(level =="language")
 
-Glottolog %>%
-  filter(!is.na(Family_name)) %>%
-  filter(Family_name != 'Bookkeeping') %>% 
-  filter(Family_name != 'Unattested') %>%
-  filter(Family_name != 'Artificial Language') %>% 
-  filter(Family_name != 'Unclassifiable') %>% 
-  filter(Family_name != 'Speech Register')  -> Glottolog
+##calcualting unique distinct color for each family (isolates all get same)
+
+n <- length(unique(Glottolog_only_languages$Family_ID))
+
+color_vector <- distinctColorPalette(n)
+
+Glottolog_only_languages$Family_color <- color_vector[as.factor(Glottolog_only_languages$Family_ID)]
+
 
 #shifting the longlat of the dataframe to match the pacific centered map
-Glottolog_long_shifted <- Glottolog %>% 
+Glottolog_long_shifted <- Glottolog_only_languages %>% 
   mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude))
-
 
 #Basemap
 basemap <- ggplot(Glottolog_long_shifted ) +
@@ -45,13 +65,9 @@ basemap <- ggplot(Glottolog_long_shifted ) +
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks = element_blank())   +
-  coord_map(projection = "vandergrinten") +
-
-
+  coord_map(projection = "vandergrinten") 
 
 png("plots/Glottolog_all_languages.png", width =1200, height = 700)
-plot(basemap + geom_point(stat = "identity", size = 2.5,aes(x=Longitude, y=Latitude), fill = Glottolog_long_shifted$Family_color , shape = 21, alpha = 0.4, stroke = 0.4, color = "grey44") +   
+plot(basemap + geom_point(stat = "identity", size = 2.5,aes(x=Longitude, y=Latitude), fill = Glottolog_long_shifted$Family_color, shape = 21, alpha = 0.4, stroke = 0.4, color = "grey44") +   
        theme(title  = element_text(size = 32)) )
 dev.off()
-
-
