d586d441edc5f04152e1bce338d7bdaa2e91191d
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-03-22 10:06:52 +0100

    excluding pidgins

--- Counting_language_names.R
+++ Counting_language_names.R
@@ -6,10 +6,11 @@ glottolog_lookup_table %>%
   filter(level == "language") %>% 
   filter(Family_name != 'Bookkeeping') %>% 
   filter(Family_name != 'Unattested') %>% 
+  filter(Family_name != 'Pidgin') %>%
   filter(Family_name != 'Artificial Language') %>% 
   filter(Family_name != 'Speech Register')  %>% 
   mutate(Name_split = str_split(Name, " ")) %>% 
-  unnest() %>%  
+  unnest(Name_split) %>%  
   group_by(Name_split) %>% 
   summarise(n = n()) %>% 
   arrange(desc(n)) %>% 
